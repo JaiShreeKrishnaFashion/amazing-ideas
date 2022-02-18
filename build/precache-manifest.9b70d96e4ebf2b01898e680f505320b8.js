bfb216ac5791f5ffa4de5e26187abbb72b06c4b6
deepak <jaishrikrishana2022@gmail.com>
2022-02-19 03:09:20 +0530

    build

---
+++ build/precache-manifest.9b70d96e4ebf2b01898e680f505320b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e30851f52ef55d994e1ed524dc056a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca1e008ccb7c28093038",
+    "url": "/static/js/2.9e10b796.chunk.js"
+  },
+  {
+    "revision": "dc8af572ed5e2401e323",
+    "url": "/static/js/main.90995314.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
