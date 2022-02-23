af67f49cd2e2f71712f8a345e12d63c9bcf76498
deepak <jaishrikrishana2022@gmail.com>
2022-02-23 15:47:23 +0530

    unit test added

---
+++ build/precache-manifest.fdc91c87b42858f7f05ee319091d1a08.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eca6d1c258f53af84d64c5f3c476e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca1e008ccb7c28093038",
+    "url": "/static/js/2.9e10b796.chunk.js"
+  },
+  {
+    "revision": "2be1f1fd08590677f17b",
+    "url": "/static/js/main.890ba4fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
